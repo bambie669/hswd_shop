0226785ce3e9b2d609dda88ceb16c154bfba56ba
bambie669 <raduroman94@gmail.com>
2020-04-09 23:35:55 +0300

    added middleware for main components

--- app.js
+++ app.js
@@ -2,16 +2,43 @@ const express = require('express');
 
 const shop = express();
 
-shop.use((req, res, next) => {
-    console.log('first middleware');
-    next();
+
+
+shop.use('/whatsnew',(req, res, next) => {
     
+    res.send('<p> the midd that handles whatsnew</p>')
+});
+
+shop.use('/accesorii',(req, res, next) => {
+    
+    res.send('<p> the midd that handles accesorii</p>')
+});
+
+shop.use('/bestbuy',(req, res, next) => {
+    
+    res.send('<p> the midd that handles bestbuy</p>')
+});
+
+shop.use('/about',(req, res, next) => {
+    
+    res.send('<p> the midd that handles about</p>')
+});
+
+shop.use('/blog',(req, res, next) => {
+    
+    res.send('<p> the midd that handles blog</p>')
+});
+
+shop.use('/sales',(req, res, next) => {
+    
+    res.send('<p> the midd that handles sales</p>')
+});
+
+shop.use('/',(req, res, next) => {
+    
+    res.send('<p> the midd that handles main app</p>')
 });
 
 
-shop.use((req, res, next) => {
-    console.log('sec midd')
-    res.send('<h1>Hello from express</h1>');
-});
 
 shop.listen(3080)
