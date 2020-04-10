75448972239c5648ec614d3ee972f06a871234a1
bambie669 <raduroman94@gmail.com>
2020-04-10 03:25:56 +0300

    admin route updated

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router();
 
 
-router.get('/add-product', (req, res, next) => {
+router.get('admin/add-product', (req, res, next) => {
     res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Adauga Produs</button></form>')
 })
 
-router.post('/product',(req, res,next) => {
+router.post('admin/product',(req, res,next) => {
     console.log(req.body);
     res.redirect('/');
     
