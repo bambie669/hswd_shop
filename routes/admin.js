12f6d054942c268cdc4984b8e8ee4b04353e69cc
bambie669 <raduroman94@gmail.com>
2020-04-10 03:26:52 +0300

    form action updated

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 router.get('admin/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Adauga Produs</button></form>')
+    res.send('<form action="/add-product" method="post"><input type="text" name="title"><button type="submit">Adauga Produs</button></form>')
 })
 
 router.post('admin/product',(req, res,next) => {
